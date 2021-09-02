0a579774ed8f86551884f71f9381abb8a0c61a06
Ayushktiwari <ayushkumar.tiwari@knoldus.com>
2021-09-02 11:10:57 +0530

    Adding Queue and Stack Implementation

---
+++ src/main/scala/QueueImplementation.scala
@@ -0,0 +1,55 @@
+class QueueImplementation {
+  var queue:List[Double] = List.empty
+  var front: Int = -1
+  var rear: Int = -1
+
+  def enqueue(item: Double): Unit = {
+    if(rear == -1 && front == -1) {
+      front = front + 1
+      rear = rear + 1
+      queue = queue ::: List(item)
+      println("Item is enqueued.")
+    }
+    else{
+      rear = rear + 1
+      queue = queue ::: List(item)
+      println("Item is enqueued.")
+    }
+  }
+
+  def dequeue: Unit = {
+    if(front == -1 && rear == -1){
+     println("Underflow..")
+    }
+    else if(front == rear ){
+      queue=queue.drop(1)
+      front = -1
+      rear = -1
+      println("Item dequeued.")
+    }
+    else{
+      queue = queue.drop(1)
+      front = front + 1
+      println("Item dequeued..")
+    }
+  }
+
+  def getQueue: Unit = {
+    println(queue)
+  }
+}
+
+object Test3 {
+  def main(args: Array[String]): Unit = {
+    val queue = new QueueImplementation
+    queue.enqueue(2)
+    queue.enqueue(4)
+    queue.enqueue(7)
+    queue.enqueue(9)
+    queue.dequeue
+   // queue.dequeue
+    println("Front is " + queue.front)
+    println("Rear is " + queue.rear)
+    queue.getQueue
+  }
+}
